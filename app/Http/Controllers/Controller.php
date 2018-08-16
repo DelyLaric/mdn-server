67975dc6eecdc4ee8fb6f3e59224f326ed69a6c2
zhanglan <delylaric@gmail.com>
2018-08-16 22:16:25 +0800

    add: add validation method in base controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,23 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function viaParams($rules)
+    {
+
+        $validator = \Validator::make($this->params, $rules);
+
+        if ($validator->fails()) {
+            throw new \Illuminate\Validation\ValidationException($validator);
+        }
+
+        return $this->params;
+    }
+
+    public function via(array $rules)
+    {
+        $this->params = request(array_keys($rules));
+
+        return $this->viaParams($rules);
+    }
 }
