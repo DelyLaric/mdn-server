132445a84dd650fb19a9f8f242fd00d86992746a
zhanglan <delylaric@gmail.com>
2018-09-09 14:43:01 +0800

    处理AreaSeeder中关于data_id的bugs

--- database/seeds/AreaSeeder.php
+++ database/seeds/AreaSeeder.php
@@ -10,17 +10,22 @@ class AreaSeeder extends Seeder
     {
         $plantIds = Facades\Plants::search()->pluck('id');
         $columns = Facades\Columns::search(['table' => 'locations']);
+        $fixedColumns = $columns->where('is_fixed', true)->map(
+            function($column) { return $column->id; }
+        )->toArray();
 
         foreach ($plantIds as $plantId) {
             for ($i = 0; $i < random_int(6, 15); $i++) {
+                $areaColumns = array_unique(array_merge(
+                    $fixedColumns,
+                    $columns->random(random_int(8, 20))->pluck('id')->toArray()
+                ));
                 DB::table('areas')->insert([
                     'plant_id' => $plantId,
                     'name' => "test_area_$i",
                     'text' => "测试区域_$i",
                     'comment' => "用于测试_$i",
-                    'columns' => array_encode(
-                        $columns->random(random_int(8, 20))->pluck('id')->toArray()
-                    )
+                    'columns' => array_encode($areaColumns)
                 ]);
             }
         }
