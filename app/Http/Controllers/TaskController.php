f31c8bbc2725e0ae20bcfbf1952fe6ff6815bab3
zhanglan <delylaric@gmail.com>
2018-09-09 11:07:22 +0800

    将TaskAreaController中的方法移至TaskController中

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -83,4 +83,53 @@ class TaskController extends Controller
 
     return 'ok';
   }
+
+  public function addArea()
+  {
+    $params = $this->via([
+      'taskId' => 'required',
+      'areaId' => 'required'
+    ]);
+
+    DB::table('task_areas')->insert([
+      'task_id' => $params['taskId'],
+      'area_id' => $params['areaId']
+    ]);
+
+    return 'ok';
+  }
+
+  public function removeArea()
+  {
+    $params = $this->via([
+      'taskId' => 'required',
+      'areaId' => 'required'
+    ]);
+
+    DB::table('task_areas')
+      ->where('task_id', $params['taskId'])
+      ->where('area_id', $params['areaId'])
+      ->delete();
+
+    return 'ok';
+  }
+
+  public function updateAreaData()
+  {
+    $params = $this->via([
+      'taskId' => 'required',
+      'areaId' => 'required',
+      'locationId' => 'nullable'
+    ]);
+
+    DB::table('task_areas')
+      ->where('task_id', $params['taskId'])
+      ->where('area_id', $params['areaId'])
+      ->update(['location_id' => $params['locationId']]);
+
+    return (array) DB::table('locations')->where([
+      'area_id' => $params['areaId'],
+      'location_id' => $params['locationId']
+    ])->get()->first();
+  }
 }
--- app/Repositories/Facades/TaskAreas.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace App\Repositories\Facades;
-
-class TaskAreas extends BaseFacade
-{
-
-}
