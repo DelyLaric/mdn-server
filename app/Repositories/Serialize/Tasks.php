c23f2cfd78d9cc75ce8dd9b7c5b48bf2b748a431
zhanglan <delylaric@gmail.com>
2018-09-26 12:36:45 +0800

    重构 tasks.search Api 的数据格式

--- app/Repositories/Serialize/Tasks.php
+++ app/Repositories/Serialize/Tasks.php
@@ -4,13 +4,5 @@ namespace App\Repositories\Serialize;
 
 class Tasks
 {
-  public static function getResource(&$data)
-  {
-    foreach ($data as &$datum) {
-      $datum->part = json_decode($datum->part);
-      $datum->areas = json_decode($datum->areas);
-    }
 
-    return $data;
-  }
 }
